c5297035c57a5e38616accb1d19346f5b083c59d
KAITO-0623 <kuidouyiteng233@gmail.com>
2024-07-17 13:48:55 +0900

    イベント発火　基礎編

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-
 window.addEventListener("load", function(){
-  const lists = document.getElementById("lists")
-    lists.addEventListener("mouseover", function(){
-      console.log("乗りました！")
-    const lists = document.getElementById('lists')
-    lists.addEventListener("mouseout", function(){
-      console.log('外れました！')
-    })
-    lists.addEventListener('click',function(){
-      console.log('クリックされました！');
-    })
-  });
+  const pullDownButton = document.getElementById("lists")
+
+    pullDownButton.addEventListener("mouseover", function(){
+      pullDownButton.setAttribute("style", "background-color:blue;")
+      console.log("乗った時は青色") 
+
+    pullDownButton.addEventListener('click',function(){
+      pullDownButton.setAttribute("style", "background-color:green;")
+      console.log('クリック時は緑色')
+
+    pullDownButton.addEventListener("mouseout", function(){
+      pullDownButton.setAttribute("style", "background-color:red;")
+      console.log('外れた時は赤色')
+    });
+    });
+    });
 });
 
