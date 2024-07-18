214785c0707d7dc0ea6f007f424cd1d69c68a8fe
KAITO-0623 <kuidouyiteng233@gmail.com>
2024-07-18 14:49:45 +0900

    条件分岐編

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 window.addEventListener("load", function(){
   const pullDownButton = document.getElementById("lists")
+  const pulldownParents = document.getElementById("pull-down")
 
     pullDownButton.addEventListener("mouseover", function(){
-      pullDownButton.setAttribute("style", "background-color:blue;")
-      console.log("乗った時は青色") 
-
+      this.setAttribute("style", "background-color:blue;")
+      console.log("乗った時は青色")
+    });
     pullDownButton.addEventListener('click',function(){
-      pullDownButton.setAttribute("style", "background-color:green;")
-      console.log('クリック時は緑色')
+      if (pulldownParents.getAttribute("style") =="display:block;"){
+        pulldownParents.removeAttribute("style", "display:block;")
+        console.log("非表示")
+      } else {
+      pulldownParents.setAttribute("style", "display:block;")
+        console.log("表示")
+      }
+    });
 
     pullDownButton.addEventListener("mouseout", function(){
-      pullDownButton.setAttribute("style", "background-color:red;")
+      this.setAttribute("style", "background-color:red;")
       console.log('外れた時は赤色')
-    });
-    });
     });
 });
 
